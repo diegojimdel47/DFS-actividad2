8f4ee65d95e85b5201c3884ebcc724a0f81cf0f9
diegojimdel417 <diego.jimenez.del@gmail.com>
2026-01-19 18:56:26 -0600

    Primeros ejercicios

---
+++ Ejercicios/Ejercicio1.java
@@ -0,0 +1,6 @@
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        String name = "Diego";
+        System.out.println("Hola " +name);
+    }
+}
---
+++ Ejercicios/Ejericicio2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Ejericicio2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Ingresa el primer número: ");
+        int num1 = sc.nextInt();
+        System.out.print("Ingresa el segundo número: ");
+        int num2 = sc.nextInt();
+        int result = num1 + num2;
+        System.out.print("El resultado de la suma es: " +result);
+
+        sc.close();
+    }
+}
