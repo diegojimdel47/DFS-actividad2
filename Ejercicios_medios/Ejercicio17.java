9fe82d6fb6be9726dedab3d8283112f3b1ec3a9e
diegojimdel417 <diego.jimenez.del@gmail.com>
2026-01-23 12:02:32 -0600

    Parte 1 y 2 de la Actividad 2 completados

---
+++ Ejercicios_medios/Ejercicio17.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Ejercicio17 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Escibe un número dentro del intervalo: ");
+    }
+}
